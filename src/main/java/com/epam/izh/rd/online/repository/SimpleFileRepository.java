19f145ddaa0b481a6bffc68fd914048f762d30f4
Batuev Vladislav <batuew.vladisl@yandex.ru>
2021-06-08 12:11:17 +0400

    Implementation of methods

--- src/main/java/com/epam/izh/rd/online/repository/SimpleFileRepository.java
+++ src/main/java/com/epam/izh/rd/online/repository/SimpleFileRepository.java
@@ -1,5 +1,13 @@
 package com.epam.izh.rd.online.repository;
 
+import java.io.*;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+
 public class SimpleFileRepository implements FileRepository {
 
     /**
@@ -10,7 +18,32 @@ public class SimpleFileRepository implements FileRepository {
      */
     @Override
     public long countFilesInDirectory(String path) {
-        return 0;
+
+        Path pathResult = Paths.get(path);
+        if (!pathResult.isAbsolute()) {
+
+            URL url = getClass().getClassLoader().getResource(path);
+            try {
+                assert url != null;
+                Path pathAbsolut = Paths.get(url.toURI());
+                path = pathAbsolut.toString();
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }
+        }
+
+        File dir = new File(path);
+        long countFiles = 0;
+        if (dir.exists() && dir.isDirectory()) {
+            for (File innerDir: Objects.requireNonNull(dir.listFiles())) {
+                if (innerDir.isFile()) {
+                    countFiles++;
+                } else {
+                    countFiles += new SimpleFileRepository().countFilesInDirectory(innerDir.getPath());
+                }
+            }
+        }
+        return countFiles;
     }
 
     /**
@@ -21,7 +54,30 @@ public class SimpleFileRepository implements FileRepository {
      */
     @Override
     public long countDirsInDirectory(String path) {
-        return 0;
+
+        Path pathResult = Paths.get(path);
+        if (!pathResult.isAbsolute()) {
+
+            URL url = getClass().getClassLoader().getResource(path);
+            try {
+                assert url != null;
+                Path pathAbsolut = Paths.get(url.toURI());
+                path = pathAbsolut.toString();
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }
+        }
+
+        File dir = new File(path);
+        long countDirs = 1;
+        if (dir.exists() && dir.isDirectory()) {
+            for (File innerDir: Objects.requireNonNull(dir.listFiles())) {
+                if (innerDir.isDirectory()) {
+                    countDirs += new SimpleFileRepository().countDirsInDirectory(innerDir.getPath());
+                }
+            }
+        }
+        return countDirs;
     }
 
     /**
@@ -32,7 +88,15 @@ public class SimpleFileRepository implements FileRepository {
      */
     @Override
     public void copyTXTFiles(String from, String to) {
-        return;
+
+        File dirFrom = new File(from);
+        File dirTo = new File(to);
+        dirTo.getParentFile().exists();
+        try {
+            Files.copy(dirFrom.getAbsoluteFile().toPath(), dirTo.getAbsoluteFile().toPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -44,6 +108,17 @@ public class SimpleFileRepository implements FileRepository {
      */
     @Override
     public boolean createFile(String path, String name) {
+        System.setProperty("file.encoding", "UTF-8");
+        System.setProperty("sun.jnu.encoding", "UTF-8");
+        String targetFolder = "target/classes/";
+        File dir = new File(targetFolder + path);
+        dir.mkdir();
+        File file = new File(dir.getPath() + File.separator + name);
+        try {
+            return file.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return false;
     }
 
@@ -55,6 +130,17 @@ public class SimpleFileRepository implements FileRepository {
      */
     @Override
     public String readFileFromResources(String fileName) {
-        return null;
+
+        StringBuilder textFromFile = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(
+                new FileReader("src/main/resources"+File.separator+fileName))){
+            while (reader.ready()) {
+                textFromFile.append(reader.readLine());
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return textFromFile.toString();
     }
 }
